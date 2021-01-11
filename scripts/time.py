54ef5f61a94d8d87301fe95888e0eeeb673655be
NATSUMETAKAFUMI <s19C1087AA@s.chibakoudai.jp>
2021-01-11 11:46:23 +0900

    Update time.py

--- scripts/time.py
+++ scripts/time.py
@@ -8,12 +8,11 @@ dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 rospy.init_node('time')
 pub = rospy.Publisher('time_search', Int32, queue_size=1)
 rate = rospy.Rate(10)
-n = 0
 
 print(dt_now)
 
 print(dt_now.tzinfo)
 
 while not rospy.is_shutdown():
-    pub.publish(n)
+    pub.publish(dt_now)
     rate.sleep()
