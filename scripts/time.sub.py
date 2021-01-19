d9ddd94f292fe3f76f601fb31369b3ea9283a4ec
NATSUMETAKAFUMI <s19C1087AA@s.chibakoudai.jp>
2021-01-19 20:59:44 +0900

    Update time.sub.py

--- scripts/time.sub.py
+++ scripts/time.sub.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 import rospy
-from msg.msgs import Data
+from std_sgs.msg import Data
 
 def talker():
-
+   print("data : %d, time : %f" % (message.date, message.time))
+    
+if __name__ == '__main__':
+   rospy.init_node('time')
+   sub = rospy.Subscriber('time_search', Date, callback)
+   rospy.spin()
