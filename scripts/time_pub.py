ba64e95f0ffd73d97b2b74915408cbccaf0d079c
NATSUMETAKAFUMI <s19C1087AA@s.chibakoudai.jp>
2021-01-19 20:48:25 +0900

    Update and rename time.py to time_pub.py

---
+++ scripts/time_pub.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rospy
+from datetime import datetime
+from std_msgs.msg import Date
+
+def talker():
+    l = []
+    d = ():
+    rate = rospy.Rate(10)
+while not rospy.is_shutdown():
+    d.data = ' '
+    l.time = ' '
+    now = datatime.now()
+    l = str(now)
+    
+    for i in range(0,10):
+        d.data += l[i]
+    for i in range(11,25):
+        d.time += l[i]
+    d.data = int(d.data.replace('_', ' '))
+    d.time = float (d.time.replace(';',' '))
+    pub.publisher(d)
+    rate.sleep()
+    
+if __name__ == '__main__':
+    rospy.init_node('time')
+    pub = rospy.Publisher('time_search', Data, queue_size=1)
+    talker()
+    rospy.spin()
