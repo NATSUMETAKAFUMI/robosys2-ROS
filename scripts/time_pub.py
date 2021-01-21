938e4dd0fe1f7a46ec541b218f9f2ec18a7eb0e7
NATSUMETAKAFUMI <s19C1087AA@s.chibakoudai.jp>
2021-01-21 09:08:51 +0900

    Update time_pub.py

--- scripts/time_pub.py
+++ scripts/time_pub.py
@@ -1,31 +1,13 @@
 #!/usr/bin/env python3
 
 import rospy
-from robosys2-ROS.msg import Date
-from datetime import datetime
+from std_msgs.msg import Float64
 
-def talker():
-    l = []
-    d = Date()
-    rate = rospy.Rate(10)
+rospy.init_node('time_pub')
+pub = rospy.Publisher('time_search', Float64, queue_size = 1)
+rate = rospy.Rate(10)
+
 while not rospy.is_shutdown():
-    d.date = ''
-    l.time = ''
-    
-    now = datetime.now()
-    l = str(now)
-    
-    for i in range(0,10):
-        d.date += l[i]
-    for i in range(11,25):
-        d.time += l[i]
-    d.date = int(d.date.replace('-', ''))
-    d.time = float(d.time.replace(':',''))
-    pub.publish(d)
+    now = rospy.get_time()
+    pub.publish(now)
     rate.sleep()
-    
-if __name__ == '__main__':
-    rospy.init_node('time_pub')
-    pub = rospy.Publisher('time_search', Date, queue_size=1)
-    talker()
-    rospy.spin()
